7ba5b7bf72cfe60e19ea880f8a42190c0ffedbb3
IMOsbo <imo2d>
2024-02-12 09:52:34 -0600

    Added F15 model :)

--- Assets/rotation.cs
+++ Assets/rotation.cs
@@ -6,6 +6,7 @@ public class rotation : MonoBehaviour
 {
     public GameObject parent;
     public GameObject plane;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +16,13 @@ public class rotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    plane.transform.position = RotatePointAroundPivot(plane.transform.position, parent.transform.position, Quaternion.Euler(0, 50 * Time.deltaTime, 0));
+    // plane.transform.position = RotatePointAroundPivot(plane.transform.position, parent.transform.position, Quaternion.Euler(0, 50 * Time.deltaTime, 0));
+    // https://docs.unity3d.com/2018.1/Documentation/ScriptReference/Transform.Rotate.html
+    parent.transform.Rotate(Vector3.up * Time.deltaTime * 100);
     }
 
+    // https://docs.unity3d.com/ScriptReference/Transform.RotateAround.html
+    // https://gamedev.stackexchange.com/questions/61981/unity3d-orbit-around-orbiting-object-transform-rotatearound#62002
     public static Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion angle) {
        return angle * ( point - pivot) + pivot;
     }
